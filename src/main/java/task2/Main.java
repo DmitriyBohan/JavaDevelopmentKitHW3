fc4717dfb9253cecb710e874df0b5ac8bb35b2bb
DmitryBohan <zorrorul@gmail.com>
2024-06-17 17:33:07 +0300

    task2 done

--- src/main/java/task2/Main.java
+++ src/main/java/task2/Main.java
@@ -6,5 +6,25 @@ package task2;
 но должны иметь одинаковую длину и содержать элементы одного типа по парно по индексам.
  */
 
+import static task2.ArrayComporator.compareArrays;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Integer[] array1 = {1, 2};
+        Integer[] array2 = {1, 2};
+        Integer[] array3 = {1, 3};
+
+        System.out.println(compareArrays(array1, array2));
+        System.out.println(compareArrays(array1, array3));
+
+        String[] array4 = {"a", "b", "c"};
+        String[] array5 = {"a", "b", "c"};
+        String[] array6 = {"a", "b", "d"};
+
+        System.out.println(compareArrays(array4, array5));
+        System.out.println(compareArrays(array4, array6));
+
+
+    }
 }
