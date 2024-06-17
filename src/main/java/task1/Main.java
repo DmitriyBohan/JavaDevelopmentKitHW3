9910e566d27f12cf2c0ebed3ed84dd43f3aba894
DmitryBohan <zorrorul@gmail.com>
2024-06-17 15:51:21 +0300

    task1 done

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -8,5 +8,21 @@ sum(), multiply(), divide(), subtract().
 Методы должны возвращать результат своей работы.
  */
 
+import static task1.Calculator.*;
+
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Sum:       "+sum(5,3));
+
+        System.out.println("Multiply:  "+ multiply(5,2.5));
+
+        System.out.println("Divide:    "+divide(5,2.5));
+
+        System.out.println("Subtract:  "+subtract(5,7));
+    }
+
+
+
+
 }
